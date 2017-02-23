7330c6970eaef48de051d3bc088afd047b656fb4
SKMKCGK <scott.kipp@gmail.com>
2017-02-23 17:04:57 -0500

    Corrects image assets display

--- app/scripts/fixtures.js
+++ app/scripts/fixtures.js
@@ -4,7 +4,7 @@ var albumPicasso = {
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
-    albumArtUrl: '/app/assets/images/album_covers/01.png',
+    albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
