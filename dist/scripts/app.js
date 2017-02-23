52c2045cd30c617cbc8ced95dd76b15363f12c0e
SKMKCGK <scott.kipp@gmail.com>
2017-02-23 16:54:53 -0500

    Updates from Assignment-4-5

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -7,15 +7,18 @@
         });
         $stateProvider
          .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
+            url: '/',
+            controller: 'LandingCtrl as landing',
+            templateUrl: '/templates/landing.html'
         })
         .state('album', {
             url: '/album',
+            controller: 'AlbumCtrl as album',
             templateUrl: '/templates/album.html'
         })
         .state('collection', {
             url: '/collection',
+            controller: 'CollectionCtrl as collection',
             templateUrl: '/templates/collection.html'
         })
     }
